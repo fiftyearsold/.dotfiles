c4266f7acf1f86fb4c1af631a3d1560e2311a752
yang <fiftyearsold@gmail.com>
2023-07-09 21:31:49 +0800

    fish config

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,7 @@ set fish_greeting
 #alias
 alias ra 'ranger'
 alias lg 'lazygit'
+alias cl 'clear'
 
 # starship
 starship init fish | source
@@ -22,4 +23,5 @@ set -gx EDITOR nvim
 
 # fzf
 set fzf_dir_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+
 set -x FZF_DEFAULT_OPTS "--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4" --cycle --layout=reverse --border --height=90% --preview-window=wrap --marker="*"
